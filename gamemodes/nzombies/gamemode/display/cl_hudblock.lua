c4d6faeff20a31eb27b03c59073ccba730b29b41
wised <14439112+Cryotheus@users.noreply.github.com>
2020-04-04 00:52:25 -0600

    Fixed a script error in display/cl_hudblock

--- gamemodes/nzombies/gamemode/display/cl_hudblock.lua
+++ gamemodes/nzombies/gamemode/display/cl_hudblock.lua
@@ -1,6 +1,6 @@
 local hud_functions = {
 	["CHudAmmo"] = function() return false end,
-	["CHudBattery"] = function() return LocalPlayer():Health() > 0 end,
+	["CHudBattery"] = function() if LocalPlayer() then return LocalPlayer():Health() > 0 end return false end,
 	["CHudWeaponSelection"] = function() return not nzRound:InProgress() and not nzRound:InState(ROUND_GO) end,
 	["CHudHealth"] = function() return GetConVar("nz_bloodoverlay"):GetBool() end,
 	["CHudSecondaryAmmo"] = function() return false end
